48bdf4e5979dafabb3e5d1080eb81f2a0a5ccd6d
Videet <vids107@gmail.com>
2023-02-16 18:32:24 +0000

    month dimension added

--- models/core/fact_fhv_trips.sql
+++ models/core/fact_fhv_trips.sql
@@ -1,18 +1,13 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-with fhv_data as (
-    select *, 
-        'fhv' as service_type 
-    from {{ ref('stg_fhv_tripdata') }}
-), 
+with
+    fhv_data as (select *, 'fhv' as service_type from {{ ref("stg_fhv_tripdata") }}),
 
 
 
-dim_zones as (
-    select * from {{ ref('dim_zones') }}
-    where borough != 'Unknown'
-)
-select fhv_data.tripid,
+    dim_zones as (select * from {{ ref("dim_zones") }} where borough != 'Unknown')
+select
+    fhv_data.tripid,
     fhv_data.dispatching_base_num,
     fhv_data.affiliated_base_number,
     fhv_data.pickup_locationid,
@@ -21,11 +16,15 @@ select fhv_data.tripid,
     fhv_data.dropoff_datetime,
     fhv_data.sr_flag,
     fhv_data.service_type,
-    fhv_data.pickup_month,
-    fhv_data.dropoff_month
-  
+    extract(month from pickup_datetime) as pickup_month,
+    extract(month from dropoff_datetime) as dropoff_month
+
+
+
+
+
 from fhv_data
-inner join dim_zones as pickup_zone
-on fhv_data.pickup_locationid = pickup_zone.locationid
-inner join dim_zones as dropoff_zone
-on fhv_data.dropoff_locationid = dropoff_zone.locationid
+inner join
+    dim_zones as pickup_zone on fhv_data.pickup_locationid = pickup_zone.locationid
+inner join
+    dim_zones as dropoff_zone on fhv_data.dropoff_locationid = dropoff_zone.locationid
